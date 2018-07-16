bc01a8247f0ca2cf54ab596d75f77d44f40b04a8
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2018-07-16 09:33:04 +0200

    Cleanup python environment overlay

--- overlays/env-python.nix
+++ overlays/env-python.nix
@@ -5,14 +5,7 @@ self: super:
     paths = [
       self.python3
       self.pipenv
-      # self.python3Packages.autopep8
-      # self.python3Packages.elpy
-      # self.python3Packages.flake8
-      # self.python3Packages.ipython
-      # self.python3Packages.jedi
       self.python3Packages.pip
-      # self.python3Packages.service_factory
-      # self.python3Packages.setuptools
     ];
     extraOutputsToInstall = [ "man" "doc" ];
   };
