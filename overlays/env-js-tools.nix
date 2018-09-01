cf8f462b07e209e4f056ecdf6dd896914eb9418c
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2018-09-01 17:52:34 +0200

    Disable all nodejs packages

--- overlays/env-js-tools.nix
+++ overlays/env-js-tools.nix
@@ -4,13 +4,13 @@ self: super:
     name = "jsTools";
     paths = [
       self.nodejs-10_x
-      self.nodePackages_6_x.eslint_d
-      # self.nodePackages_6_x.import_js # Not yet packaged in master
-      self.nodePackages_6_x.prettier
-      # self.nodePackages_6_x.reveal-md # Not yet packaged in master
-      self.nodePackages_6_x.tern
-      # self.nodePackages_6_x.tern-jsx  # Not yet packaged in master
-      self.nodePackages_6_x.typescript
+      # self.nodePackages_8_x.eslint_d
+      # self.nodePackages_8_x.import_js # Not yet packaged in master
+      # self.nodePackages_8_x.prettier
+      # self.nodePackages_8_x.reveal-md # Not yet packaged in master
+      # self.nodePackages_8_x.tern
+      # self.nodePackages_8_x.tern-jsx  # Not yet packaged in master
+      # self.nodePackages_8_x.typescript
       # self.phantomjs2                 # Not yet packaged for darwin
       self.yarn
     ];
