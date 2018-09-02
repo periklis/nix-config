f3271b3bfed9a64619b83a27c05ad4943fda0101
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2018-09-02 21:14:51 +0200

    Improve tmux status bar and keys

--- dot-files/tmux.nix
+++ dot-files/tmux.nix
@@ -7,9 +7,28 @@ set -g visual-activity off
 set -g visual-bell off
 set -g visual-silence off
 set -g bell-action none
+set -g mode-keys emacs
+set -g status-keys emacs
+set -g status-interval 5
+setw -g aggressive-resize on
+set -g base-index 1
+set -g pane-base-index 1
+set -g prefix C-z
 
-bind | split-window -h
-bind - split-window -v
+# length of tmux status line
+set -g status-left-length 30
+set -g status-right-length 150
+
+set -g pane-active-border-fg colour8
+set -g status-position bottom
+set -g status-left '#[bg=colour8]#[fg=colour7] #S #[bg=colour3]#[fg=colour8] '
+set -g status-right '#[fg=colour8]#[bg=colour3]#[fg=colour7]#[bg=colour8] #H #[bg=colour3]#[fg=colour0] %a %D %T '
+
+# Set the background color
+set -g status-bg colour3
+
+bind | split-window -h -c "#{pane_current_path}"
+bind - split-window -v -c "#{pane_current_path}"
 unbind '"'
 unbind %
 ''
