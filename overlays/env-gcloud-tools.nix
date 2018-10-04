7539842f7ab65e0d5a3106d47a406b45d649ffc4
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2018-10-04 13:26:32 +0200

    Add ops related tools to gcloud overlay

--- overlays/env-gcloud-tools.nix
+++ overlays/env-gcloud-tools.nix
@@ -4,10 +4,13 @@ self: super:
     name = "gcloudTools";
     paths = [
       self.heptio-ark
+      self.jq
       self.kubectx
       self.kubernetes
       self.minikube
+      self.netcat
       self.google-cloud-sdk
+      self.packer
     ];
     extraOutputsToInstall = [ "doc" "info ""man" ];
   };
