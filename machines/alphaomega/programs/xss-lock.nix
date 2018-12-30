11e6afdd540662772d6e927164d2469df72831fa
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2018-12-30 11:42:31 +0100

    [alphaomega] Lock ssh-agent on suspend/hibernate

--- machines/alphaomega/programs/xss-lock.nix
+++ machines/alphaomega/programs/xss-lock.nix
@@ -2,6 +2,6 @@
 {
   programs.xss-lock = {
     enable = true;
-    lockerCommand = "${pkgs.i3lock-fancy}/bin/i3lock-fancy";
+    lockerCommand = "${pkgs.i3lock-fancy}/bin/i3lock-fancy && ${pkgs.openssh}/bin/ssh-add -x";
   };
 }
