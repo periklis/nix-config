f344fd92686358cc361a5bffd13039a09e808f7a
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2019-04-07 11:28:25 +0200

    [alphaomega] Improve power consumption

--- machines/alphaomega/services/udev.nix
+++ machines/alphaomega/services/udev.nix
@@ -5,5 +5,17 @@
 
     # power autocontrol for all devices
     ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"
+
+    # power off usb smart card reader
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="058f", ATTR{idProduct}=="9540", TEST=="power/control", ATTR{power/control}="off"
+
+    # power off usb smart card reader
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="0316", TEST=="power/control", ATTR{power/control}="off"
+
+    # power off integraged IR camera
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="04f2", ATTR{idProduct}=="b613", TEST =="power/control", ATTR{power/control}="off"
+
+    # power off fibocom wwan device
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="2cb7", ATTR{idProduct}=="0210", TEST =="power/control", ATTR{power/control}="off"
   '';
 }
