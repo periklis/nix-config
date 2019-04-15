3cd5168490d6627862770243f042798de0d12638
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2019-04-15 14:03:26 +0200

    [nix-tools] Drop unused tools

--- overlays/env-nix-tools.nix
+++ overlays/env-nix-tools.nix
@@ -3,7 +3,6 @@ self: super:
   nixToolsEnv = super.buildEnv {
     name = "nixTools";
     paths = [
-      self.nixops
       self.nix-prefetch-git
       self.nix-prefetch-github
     ];
@@ -15,7 +14,6 @@ self: super:
       self.nix-index
       self.nix-review
       self.nix-top
-      # self.vulnix
       self.nix-delegate
       self.nix-deploy
       self.nix-diff
