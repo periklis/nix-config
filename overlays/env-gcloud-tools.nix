4b9cd26b7de82e84baa7c34034c3853f0892541c
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2019-04-15 14:02:16 +0200

    [gcloud-tools] drop self-baked tools for gcloud ones

--- overlays/env-gcloud-tools.nix
+++ overlays/env-gcloud-tools.nix
@@ -3,13 +3,9 @@ self: super:
   gcloudToolsEnv = super.buildEnv {
     name = "gcloudTools";
     paths = [
-      # self.heptio-ark # Disable for our cluster for now
       self.jq
       self.kubectx
-      self.kubernetes
       self.kustomize
-      self.minikube
-      self.google-cloud-sdk
       self.packer
       self.stern
       self.vault
--- overlays/env-python.nix
+++ overlays/env-python.nix
@@ -3,6 +3,7 @@ self: super:
   pythonToolsEnv = super.buildEnv {
     name = "pythonTools";
     paths = [
+      self.python2
       self.python3
       # self.pipenv # Pending https://github.com/NixOS/nixpkgs/issues/51034
       self.python3Packages.pip
