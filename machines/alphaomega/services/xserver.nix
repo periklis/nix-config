96e9d7de58ade5897e2d6f58e142a0492b284437
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2019-07-04 21:49:59 +0200

    [alphaomega] Switch i3 for gnome3

--- machines/alphaomega/services/xserver.nix
+++ machines/alphaomega/services/xserver.nix
@@ -1,43 +1,15 @@
 {config, pkgs, ...}:
-let
-  keymap = pkgs.writeText "keymap.xkb" ''
-    xkb_keymap {
-        xkb_keycodes  { include "evdev+aliases(qwerty)" };
-        xkb_types     { include "complete"  };
-        xkb_compat    { include "complete"  };
-        xkb_symbols   {
-            include "pc+us+gr(extended):2+inet(evdev)+group(alt_space_toggle)+ctrl(nocaps)+eurosign(e)+terminate(ctrl_alt_bksp)"
-            key <AC01> { [ a, A, adiaeresis, Adiaeresis ] };
-            key <AC02> { [ s, S, ssharp, U03A3 ] };
-            key <AD09> { [ o, O, odiaeresis, Odiaeresis ] };
-            key <AD07> { [ u, U, udiaeresis, Udiaeresis ] };
-            include "level3(ralt_switch)"
-        };
-        xkb_geometry  { include "pc(pc105)" };
-    };
-  '';
-in
 {
-  environment.etc."X11/keymap.xkb".source = keymap;
-
   services.xserver = {
     enable = true;
     enableCtrlAltBackspace = true;
 
-    dpi = 192;
-
-    displayManager.lightdm = {
-      enable = true;
-      background = "${pkgs.nixos-artwork.wallpapers.stripes-logo}/share/artwork/gnome/nix-wallpaper-stripes-logo.png";
-    };
-
-    displayManager.sessionCommands = ''
-      ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${keymap} $DISPLAY
-    '';
+    displayManager.lightdm.enable = true;
 
     desktopManager = {
       default = "none";
       xterm.enable = false;
+      gnome3.enable = true;
     };
 
     inputClassSections = [
@@ -64,7 +36,5 @@ in
     useGlamor = true;
 
     videoDrivers = [ "intel" ];
-
-    windowManager.i3.enable = true;
   };
 }
