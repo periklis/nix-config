80a89d06c7fd13eb83049aff6446ac65af7a0203
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2019-07-27 19:10:20 +0200

    [alphaomega] Switch to tmux powerline blue theme

--- machines/alphaomega/dot-files/tmux.nix
+++ machines/alphaomega/dot-files/tmux.nix
@@ -9,27 +9,76 @@ set -g visual-bell off
 set -g visual-silence off
 set -g bell-action none
 setw -g mode-keys emacs
-set -g history-limit 5000
+set -g history-limit 10000
 set -g status-keys emacs
-set -g status-interval 5
 set -g base-index 1
 set -g pane-base-index 1
 set -g prefix C-z
 
-# Set the length of the right status line
+#
+# Powerline Blue - Tmux Theme
+# Created by Jim Myhrberg <contact@jimeh.me>.
+#
+# Inspired by vim-powerline: https://github.com/Lokaltog/powerline
+#
+# Requires terminal to be using a powerline compatible font, find one here:
+# https://github.com/Lokaltog/powerline-fonts
+#
+
+# Status update interval
+set -g status-interval 1
+
+# Basic status bar colors
+set -g status-style fg=colour238,bg=colour233
+
+# Left side of status bar
+set -g status-left-style bg=colour233,fg=colour243
+set -g status-left-length 40
+set -g status-left "#[fg=colour232,bg=colour24,bold] #S #[fg=colour24,bg=colour240,nobold]#[fg=colour233,bg=colour240] #(whoami) #[fg=colour240,bg=colour235]#[fg=colour240,bg=colour235] #I:#P #[fg=colour235,bg=colour233,nobold]"
+
+# Right side of status bar
+set -g status-right-style bg=colour233,fg=colour243
 set -g status-right-length 150
+set -g status-right "#[fg=colour235,bg=colour233]#[fg=colour240,bg=colour235] %H:%M:%S #[fg=colour240,bg=colour235]#[fg=colour233,bg=colour240] %d-%b-%y #[fg=colour245,bg=colour240]#[fg=colour233,bg=colour240,bold] #(${bash}/bin/bash ~/.tmux/plugins/kube.tmux) "
 
-# Set the background color of the status line
-set -g status-bg black
+# Window status
+set -g window-status-format "  #I:#W#F  "
+set -g window-status-current-format "#[fg=colour233,bg=black]#[fg=colour33,nobold] #I:#W#F #[fg=colour233,bg=black,nobold]"
 
-# Setup left and right status lines
-set -g status-left "#[fg=black,bg=colour3,bold] #S #[fg=colour3,bg=black,nobold,noitalics,nounderscore]"
-set -g status-right "#(${bash}/bin/bash ~/.tmux/plugins/kube.tmux white white white)"
+# Current window status
+set -g window-status-current-style bg=colour100,fg=colour235
 
-# Set window status format
-set -g window-status-format "#[fg=black,bg=brightblack,nobold,noitalics,nounderscore] #[fg=white,bg=brightblack]#I #[fg=white,bg=brightblack,nobold,noitalics,nounderscore] #[fg=white,bg=brightblack]#W #F #[fg=brightblack,bg=black,nobold,noitalics,nounderscore]"
-set -g window-status-current-format "#[fg=black,bg=colour3,nobold,noitalics,nounderscore] #[fg=black,bg=colour3]#I #[fg=black,bg=colour3,nobold,noitalics,nounderscore] #[fg=black,bg=colour3]#W #F #[fg=colour3,bg=black,nobold,noitalics,nounderscore]"
+# Window with activity status
+set -g window-status-activity-style bg=colour233,fg=colour245
+
+# Window separator
 set -g window-status-separator ""
+
+# Window status alignment
+set -g status-justify left
+
+# Pane border
+set -g pane-border-style bg=default,fg=colour238
+
+# Active pane border
+set -g pane-active-border-style bg=default,fg=colour24
+
+# Pane number indicator
+set -g display-panes-colour colour233
+set -g display-panes-active-colour colour245
+
+# Clock mode
+set -g clock-mode-colour colour24
+set -g clock-mode-style 24
+
+# Message
+set -g message-style bg=colour24,fg=black
+
+# Command message
+set -g message-command-style bg=colour233,fg=black
+
+# Mode
+set -g mode-style bg=colour24,fg=colour231
 
 bind | split-window -h -c "#{pane_current_path}"
 bind - split-window -v -c "#{pane_current_path}"
