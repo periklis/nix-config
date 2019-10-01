f5735d20b35b7b7eae4aad68acac522530263663
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2019-10-01 08:35:43 +0200

    [alphaomega] Cleanup configuration

--- machines/alphaomega/configuration.nix
+++ machines/alphaomega/configuration.nix
@@ -70,7 +70,6 @@
     enableGhostscriptFonts = true;
     fontconfig = {
       enable = true;
-      # dpi = 144;
       antialias = true;
       hinting.enable = true;
     };
@@ -115,5 +114,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.09"; # Did you read the comment?
+  system.stateVersion = "19.03"; # Did you read the comment?
 }
